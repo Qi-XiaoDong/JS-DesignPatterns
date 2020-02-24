4eded59f3d1a5e23ed5dfa208dce48de0d74d918
Qi-XiaoDong <1227906695@qq.com>
2020-02-24 15:25:47 +0800

    通过操作原生Function给所有函数和通过构造函数生成的对象添加方法

---
+++ index2.js
@@ -0,0 +1,47 @@
+// Function.prototype.text1 = function () {
+//   console.log("text1");
+// }
+// Function.prototype.text2 = function () {
+//   console.log("text2")
+// }
+
+// var text = function () {};
+// var Text = new Function ();
+
+// text.text1();
+
+
+// Function.prototype.addfun = function (name, callback) {
+//   this[name] = callback;
+//   return this; // 链式添加
+// }
+
+
+// var text = function() {};
+// var Text = new Function();
+// text.addfun('text1', function () {
+//   console.log(1);
+//   return this;  //l链式调用
+// }).addfun('text2', function () {
+//   console.log(2);
+//   return this;
+// });
+
+// Function.prototype.addfun = function (name, callback) {
+//   this.prototype[name] = callback;
+//   return this; // 链式调用
+// }
+
+// var Text = function() {};
+
+// Text.addfun('text1', function () {
+//   console.log(1);
+// }).addfun('text2', function () {
+//   console.log(2);
+// });
+
+// var text = new Text();
+
+
+
+
